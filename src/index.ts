98be957de5365876d887455a600a921131eead61
Hongtian Wang <oracynx@gmail.com>
2025-09-30 16:14:14 +0800

    fix(index.ts): add main

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-async () =>
+async function main()
 {
     (document.getElementById('hiddenCheckCode') as HTMLInputElement).value = '';
     (document.getElementById('checkCode') as HTMLInputElement).hidden = true;
     (document.getElementById('codeImg') as HTMLInputElement).hidden = true;
 }
+
+main();
